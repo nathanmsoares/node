fe6774d7a8e2b893bcb21ea12d6145c79f3f8ed7
Nathan Mainka Soares <nathan.soares.ns@gmail.com>
2021-07-14 19:35:14 +0000

    Node Module Done

--- projeto/src/bancoDeDados.js
+++ projeto/src/bancoDeDados.js
@@ -3,9 +3,7 @@ const sequence = {
     get id() { return this._id++ }
 }
 
-const produtos = {
-
-}
+const produtos = {}
 
 function salvarProduto(produto) {
     if (!produto.id) produto.id = sequence.id
@@ -20,3 +18,12 @@ function getProduto(id) {
 function getProdutos() {
     return Object.values(produtos)
 }
+
+function excluirProduto(id) {
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
+}
+
+
+module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto}
